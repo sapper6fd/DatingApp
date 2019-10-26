333c24eb8603eebae4d7cd00d9088754ca641e14
Justin Gallow <justingallow@Justins-MacBook-Pro.local>
2019-10-25 23:19:59 -0600

    Initial Commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,12 @@
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        // Creates the models (also known as entities) for the DatingApp that represent the values stored in the database
+
+        // ID of the table
+        public int Id { get; set; }
+        // Name of the table
+        public string Name { get; set; }
+    }
+}
