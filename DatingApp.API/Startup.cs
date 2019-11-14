939c5a564b7e9443245c01cb6ff8c7df1360ab0f
Justin Gallow <justingallow@Justins-MacBook-Pro.local>
2019-11-14 13:31:26 -0700

    End of section 11

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -43,6 +43,7 @@ namespace DatingApp.API
                         Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper(typeof(DatingRepository).Assembly);
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>();
